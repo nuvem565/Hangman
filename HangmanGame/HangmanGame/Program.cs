b7a602dd898f51ce4b6e4e407bea55f4df96bef9
nuvem565 <tom.jedrzejewski565@gmail.com>
2020-11-18 14:05:02 +0100

    Array of european countries and the filter
    Variable with list of european countries applied to a function which filters the input array of countries from all over the world. Last two lines for debug.

--- HangmanGame/HangmanGame/Program.cs
+++ HangmanGame/HangmanGame/Program.cs
@@ -11,12 +11,71 @@ namespace HangmanGame
     {
         static void Main(string[] args)
         {
+            // import the array of countries with capitals
             string workingDirectory = Environment.CurrentDirectory;
             string countriesFilePath = Directory.GetParent(workingDirectory).Parent.FullName + "\\" + "countries_and_capitals.txt";
             string[] countriesInput = File.ReadAllLines(countriesFilePath);
 
-            //for(int i = 0; i < countriesInput.Length; i++)
-            //    Console.Write(countriesInput[i] + "\r\n");
+            for(int i = 0; i < countriesInput.Length; i++)
+                Console.Write(countriesInput[i] + "\r\n");
+
+            // the array of european countries
+            string[] europeanCountries = new string[] {
+                "Albania",
+                "Andorra",
+                "Armenia",
+                "Austria",
+                "Belarus",
+                "Belgium",
+                "Bosnia and Herzegovina",
+                "Bulgaria",
+                "Croatia",
+                "Cyprus",
+                "Czech Republic",
+                "Denmark",
+                "Estonia",
+                "Finland",
+                "France",
+                "Germany",
+                "Greece",
+                "Hungary",
+                "Iceland",
+                "Ireland",
+                "Italy",
+                "Kosovo",
+                "Latvia",
+                "Liechtenstein",
+                "Lithuania",
+                "Luxembourg",
+                "Macedonia",
+                "Malta",
+                "Moldova",
+                "Monaco",
+                "Montenegro",
+                "The Netherlands",
+                "Norway",
+                "Poland",
+                "Portugal",
+                "Romania",
+                "Russia",
+                "San Marino",
+                "Serbia",
+                "Slovakia",
+                "Slovenia",
+                "Spain",
+                "Sweden",
+                "Switzerland",
+                "Ukraine",
+                "United Kingdom",
+                "Vatican City" };
+
+            // filtering european countries
+            string[] europeans =
+                Array.FindAll(countriesInput, x =>
+                    europeanCountries.Any(anyEuropeanCountry => x.StartsWith(anyEuropeanCountry))
+                );
+            for (int i = 0; i < europeans.Length; i++)
+                Console.Write(europeans[i] + "\r\n");
         }
     }
 }
